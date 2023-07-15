940db5e316c4010992663bbc998a994b83862d4c
marcelo.lima4 <marcelo.lima@fvj.br>
2023-07-15 14:49:27 -0300

    Bugfix

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  API_PATH:"https://api.themoviedb.org/3/movie/",
+  API_KEY:"api_key=87998615be34535fc02f2417a2f6c62f",
+  API_SEARCH:"https://api.themoviedb.org/search/movie/"
 };
