c466b14f0432c7f758ea4962b6ba88c0311e2853
marcelo.lima4 <marcelo.lima@fvj.br>
2023-07-15 11:03:09 -0300

    Config environments variables

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  API_PATH:"https://api.themoviedb.org/3/movie/",
+  API_KEY:"api_key=87998615be34535fc02f2417a2f6c62f",
+  API_SEARCH:"https://api.themoviedb.org/search/movie/"
+};
