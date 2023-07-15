1db37c0425a8bf9416877b4f454f637f369df88c
marcelo.lima4 <marcelo.lima@fvj.br>
2023-07-15 20:25:16 -0300

    bugfixes

--- src/app/components/top-movies/top-movies.component.ts
+++ src/app/components/top-movies/top-movies.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { IMovie } from 'src/interfaces/imovie';
-import { MovieService } from 'src/services/movie.service';
 
 @Component({
   selector: 'app-top-movies',
@@ -8,22 +6,5 @@ import { MovieService } from 'src/services/movie.service';
   styleUrls: ['./top-movies.component.css']
 })
 export class TopMoviesComponent {
-  movies: IMovie[] = [];
-  topMovies: IMovie[] = [];
 
-  constructor(private movieService: MovieService) {}
-
-  ngOnInit() {
-    this.getAllMovies();
-  }
-
-  getAllMovies() {
-    this.movieService.getAll()
-      .then(movies => {
-        movies?.sort((a, b) => b.popularity - a.popularity);
-        this.topMovies = movies?.slice(0, 10) || [];
-        console.log(this.topMovies);
-      })
-      .catch(error => console.error(error));
-  }
 }
